c9161789a3f7ca1118bb21966f62a29d906dd8a6
Yusuf Emir USTAOĞLU <emiru893@gmail.com>
2025-02-22 13:29:53 +0300

    Geliştirme

--- src/Console/Commands/Migrate.php
+++ src/Console/Commands/Migrate.php
@@ -2,7 +2,23 @@
 
 namespace NovaCore\Console\Commands;
 
+use NovaCore\Database\MigrationManager;
+
 class Migrate
 {
+    public function handle(): void
+    {
+        global $argv;
+        $manager = new MigrationManager(BasePath . "database/migrations", array());
+        if ($argv[2] == "up") {
+            $manager->migrateUp();
+        } else {
+            $manager->migrateDown();
+        }
+    }
 
+    public static function getDescription(): string
+    {
+        return "Veritabanı eşitlemesini yapar.";
+    }
 }
--- src/Console/Commands/Serve.php
+++ src/Console/Commands/Serve.php
@@ -8,10 +8,18 @@ class Serve
 {
     public function handle(): void
     {
+        echo "\e[31m
+  _   _                 ____  _           
+ | \ | | _____   ____ _|  _ \| |__  _ __  
+ |  \| |/ _ \ \ / / _` | |_) | '_ \| '_ \ 
+ | |\  | (_) \ V / (_| |  __/| | | | |_) |
+ |_| \_|\___/ \_/ \__,_|_|   |_| |_| .__/ 
+                                   |_|    
+\e[0m";
         $host = $option ?? '127.0.0.1:1108'; // Eğer bir IP verilmemişse varsayılan değer
         $command = "php -S $host -t public";
         exec($command);
-        echo "[" . date("Y-m-d H:i:s") . "] PHP " . Helper::phpSurum(2) . " Development Server (" . $host . ") started";
+        echo "[" . date("Y-m-d H:i:s") . "] PHP " . Helper::phpSurum(2) . " NovaPhp v1.0.0 Development Server (" . $host . ") started";
     }
 
     public static function getDescription()
