a1004b47614f465dea1039089879ac180e331356
Yusuf Emir USTAOĞLU <emiru893@gmail.com>
2025-03-14 00:18:12 +0300

    serve command update

--- src/Console/Commands/Serve.php
+++ src/Console/Commands/Serve.php
@@ -12,14 +12,15 @@ class Serve extends Command
 
     public function handle(): void
     {
-       echo Helper::novaPhpAsci();
+        global $argv;
+        echo Helper::novaPhpAsci();
 
-        $host = $this->argument(0) ?? '127.0.0.1:1108';
+        $host = $argv[2] ?? '127.0.0.1:1108';
         $command = "php -S $host -t public";
 
         try {
             exec($command);
-            $this->info("[" . date("Y-m-d H:i:s") . "] PHP " . Helper::phpSurum(2) . " NovaPhp v". novaVersion() ." Development Server (" . $host . ") started");
+            $this->info("[" . date("Y-m-d H:i:s") . "] PHP " . Helper::phpSurum(2) . " NovaPhp v" . novaVersion() . " Development Server (" . $host . ") started");
         } catch (\Exception $e) {
             $this->error("Server başlatma hatası: " . $e->getMessage());
         }
